2530216e37d1223fcf9a16a13165a59e38084bac
mima ch <mima.cherfa19@hotmail.fr>
2023-01-24 11:06:58 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const PluginReactComponentModal = ({ openModal, closeModal }) => {
     if (openModal) {
         console.log("modal open");
         return (
-            <div className="modal-container">
-                <div className="modal-content">
-                    <p className="close-modal" onClick={closeModal}>x</p>
+            <div className="parent-container">
+                <div className="modal-container">
+                    <div className="modal-content">
+                        <p className="close-modal" onClick={closeModal}>x</p>
+                    </div>
+                    <h2 className="titleModal">Employee Created !</h2>
                 </div>
-                <h2 className="titleModal">Employee Created !</h2>
             </div>
         );
     }
